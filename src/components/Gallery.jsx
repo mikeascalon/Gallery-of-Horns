d126dd7bd779a6c9fad89b9c81b795fa637e0ef0
Michelangelo Ascalon <mikeascalon@yahoo.com>
2023-11-28 14:30:52 -0800

    display img working

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,25 +1,56 @@
-import HornedBeast from "./HornedBeast";
 
-function Gallery() {
+// import HornedBeast from "./HornedBeast";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+
+
+function Gallery(props) {
 
     return (
         <>
-            <HornedBeast
-                title="narwhal"
-                imageUrl="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"
-                description="A unicorn and a narwhal nuzzling their horns"
-            />
-            <HornedBeast
-                title="rhino"
-                imageUrl="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80"
-                description="Parent rhino with two babies"
-            />
+            <h2>{props.message}</h2>
+            <Container>
+                <Row>
+                    <Col>
+                        <BeastImage image_url={props.allBeastData[0].image_url}/>
+                    </Col>
+                    <Col>
+                        <BeastImage image_url={props.allBeastData[1].image_url}/>
+                    </Col>
+                    <Col>
+                        <BeastImage image_url={props.allBeastData[2].image_url}/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <BeastImage image_url={props.allBeastData[3].image_url}/>
+                    </Col>
+                    <Col>
+                        <BeastImage image_url={props.allBeastData[4].image_url}/>
+                    </Col>
+                    <Col>
+                        <BeastImage image_url={props.allBeastData[5].image_url}/>
+                    </Col>
+                </Row>
+                
+
+
+            </Container>
+           
         </>
     );
 }
 
 
-
+function BeastImage(props){
+    return (
+        <div>
+            <Image src={props.image_url} alt="some horned beast" rounded fluid></Image>
+        </div>
+    )
+}
 
 
 export default Gallery;
